cbc7e072b2d7bc2854940e0cc1b4f185d3045454
Matheus Costa <matheus.costa31102002@gmail.com>
2022-04-08 21:59:25 -0300

    header terminado

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,4 +1,8 @@
-const btnDropdow = document.getElementById('buttonDropdown')
-const menuDropdown = document.getElementById('menuDropdown')
-btnDropdow.onmouseenter = () => menuDropdown.style.display = 'block'
-btnDropdow.onmouseleave = () => menuDropdown.style.display = 'none' 
+const dropdownAccount = () => {
+    const btnDropdow = document.getElementById('buttonDropdown')
+    const menuDropdown = document.getElementById('menuDropdown')
+
+    btnDropdow.onmouseenter = () => menuDropdown.style.display = 'block'
+    btnDropdow.onmouseleave = () => menuDropdown.style.display = 'none' 
+}
+dropdownAccount()
